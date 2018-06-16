6887d7f21af58ea561586d061f2013d252b4cc1c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-16 01:18:34 +0000

    automatically backed up by learn

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -12,8 +12,4 @@
 class Classroom < ActiveRecord::Base
   has_many :classroom_students
   has_many :students, through: :classroom_students
-
-  def oldest_student
-    students.where("birthday is not null").order("birthday asc").first
-  end
 end
